62d675c4a16113a45ba4b3518b1a842504ffbe56
Antonio Silva del Pozo <asilvadelpozo@gmail.com>
2014-03-27 19:09:08 +0100

    Added Test Suite (ServiceTests.java).

---
+++ guikuki-service/src/test/java/com/guikuki/service/impl/ServiceTests.java
@@ -0,0 +1,12 @@
+package com.guikuki.service.impl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by antoniosilvadelpozo on 27/03/14.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({PhotoServiceImplTest.class, RestaurantServiceImplTest.class})
+public class ServiceTests {
+}
